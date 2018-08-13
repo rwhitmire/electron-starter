a72eec771970d66ccaa6ac9cb8d10271b4d633a1
Ryan Whitmire <ryanw51@gmail.com>
2018-08-13 09:05:47 -0400

    update version

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import Layout from '../Shared/Layout';
 
 const Home = () => {
-  return <Layout>Home</Layout>;
+  return <Layout>Home v1.0.1</Layout>;
 };
 
 export default Home;
